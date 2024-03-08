1b6e32f9fe499edeeaca0e7207290db1fa3f60d0
akshaj65 <jhonsonrozario13@gmail.com>
2024-03-08 23:47:23 +0530

    Added route Comments

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,11 +4,27 @@ import isAuthenticatedUser from "../middlewares/authentication.js";
 
 const authRouter = express.Router();
 
+/**
+ * POST  /api/v1/auth/register - create user and sends a session cookie to authenticate the user.
+ */
 authRouter.post('/register', register);
+
+/**
+ * POST  /api/v1/auth/login - authenticate user by their credentials and sends a session cookie to authenticate the user.
+ */
 authRouter.post('/login', login);
+
+/**
+ * GET  /api/v1/auth/logout - Clear the session cookie to log out the user.
+ */
 authRouter.get('/logout', logout);
 
+
 //authenticated access
-authRouter.get('/me', isAuthenticatedUser,loadMe);
+
+/**
+ * GET  /api/v1/auth/me -authenticate user by their user ID
+ */
+authRouter.get('/me', isAuthenticatedUser, loadMe);
 
 export default authRouter;
--- backend/routes/pdfRoutes.js
+++ backend/routes/pdfRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {  savePdfToUser, editPdfAndSend, downloadPdfFile } from "../controllers/pdfController.js";
+import { savePdfToUser, editPdfAndSend, downloadPdfFile } from "../controllers/pdfController.js";
 import isAuthenticatedUser from "../middlewares/authentication.js";
 import { singleUpload } from "../middlewares/multer.js";
 
@@ -8,14 +8,18 @@ const pdfRouter = express.Router();
 //authenticated access
 
 /**
- * POST /pdf/upload - upload single pdf file
+ * POST  /api/v1/pdf/upload - upload single pdf file and save pdf data to user
  */
 pdfRouter.post('/upload', isAuthenticatedUser, singleUpload, savePdfToUser);
+
 /**
- * GET  /pdf/:id   download
+ * GET  /api/v1/pdf/:id   download a specific pdf by ID 
  */
 pdfRouter.get('/download/:id', isAuthenticatedUser, downloadPdfFile);
 
+/**
+ * POST /api/v1/pdf/upload - update a specific pdf by ID
+ */
 pdfRouter.post('/edit/:id', isAuthenticatedUser, editPdfAndSend);
 
 
